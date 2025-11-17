b523ab522346794f3051754b15f43bc27df0a0d7
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2025-11-17 16:36:25 +0900

    feat Background 설치
    +발판 풀링 작업중

---
+++ GameJam/Assets/Scripts/Foothold/FootholdFactory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FootholdFactory : MonoBehaviour
+{
+    private static FootholdFactory s_instance = null;
+    public static FootholdFactory Instance => s_instance;
+
+    [Header("발판 프리팹")]
+    [SerializeField] private GameObject _footholdPrefab;
+
+}
